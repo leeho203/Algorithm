0a6eb1ee11f1854a9e138380b7e9f85c1df29a44
leeho203 <leeho203@naver.com>
2016-06-02 18:03:26 +0900

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,38 @@
+package com.project.search;
+
+import java.util.Scanner;
+
+public class BinarySearch {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[]{1, 3, 4, 5, 6, 8, 10, 12, 13};
+		int num;
+		while(sc.hasNext()){
+			num = sc.nextInt();
+			System.out.println("Answer : "+binarySearch(arr, num));
+		}
+	}
+	
+	public static int binarySearch(int[] arr, int target){
+		int left = 0;
+		int right = arr.length - 1;
+		int mid;
+		
+		while(left<=right){
+			mid = (left + right)/2;
+			
+			if(target==arr[mid]){
+				return mid;
+			}else{
+				if(target<arr[mid]){
+					right = mid - 1;
+				}else{
+					left = mid + 1;
+				}
+			}
+		}
+		
+		return -1;
+	}
+	
+}

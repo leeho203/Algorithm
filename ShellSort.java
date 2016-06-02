80b4e518313f359e71177e231db8a559317b015a
leeho203 <leeho203@naver.com>
2016-06-02 17:44:57 +0900

    Update ShellSort.java

--- ShellSort.java
+++ ShellSort.java
@@ -16,7 +16,7 @@ public class ShellSort {
 		int i, j, m, insertData;
 		int interval = len;
 		while (interval != 1) {
-			interval = interval / 4 + 1;					//	interval 계속 줄어들어 1이 되면 종료
+			interval = interval / 4 + 1;							//	interval 계속 줄어들어 1이 되면 종료
 			for (m = 0; m < interval; m++) {
 				for (i = m + interval; i < len; i += interval) {	//	interval 간격으로 삽입정렬을 수행
 					insertData = arr[i];
